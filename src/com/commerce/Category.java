0cdafbbb1156b28564d98980ef3d4a0014bcf676
Schaewon2 <schaewon1206@gmail.com>
2026-03-27 15:59:16 +0900

    feat: step3, 4 단계 완료

--- src/com/commerce/Category.java
+++ src/com/commerce/Category.java
@@ -1,4 +1,23 @@
 package com.commerce;
 
+import java.util.List;
+
 public class Category {
+    // 속성
+    private List<Product> productList; // 리스트를 묶어서 카테고리가 되는 거임
+    private String categoryName;
+
+    // 카테고리
+    public Category(List<Product> productList, String categoryName) {
+        this.productList = productList;
+        this.categoryName = categoryName;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
 }
--- src/com/commerce/Main.java
+++ src/com/commerce/Main.java
@@ -1,23 +1,31 @@
 package com.commerce;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        List<Product> products = new ArrayList<Product>();
+        Scanner sc = new Scanner(System.in);
+
+        List<Category> categoryList = new ArrayList<>();
+        List<Product> electronics = new ArrayList<>();
 
         Product product1 = new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 30);
         Product product2 = new Product("iPhone 16", 1350000, "Apple의 최신 스마트폰", 15);
         Product product3 = new Product("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 20);
         Product product4 = new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 10);
 
-        products.add(product1);
-        products.add(product2);
-        products.add(product3);
-        products.add(product4);
+        electronics.add(product1);
+        electronics.add(product2);
+        electronics.add(product3);
+        electronics.add(product4);
 
-        CommerceSystem system = new CommerceSystem(products);
+        Category category1 = new Category(electronics, "전자제품");
+        categoryList.add(category1);
+
+        CommerceSystem system = new CommerceSystem(categoryList);
         system.start();
-
     }
 }
