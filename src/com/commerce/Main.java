126ae6d9fc123082fa3978f4a614ff1aab923ffc
Schaewon2 <schaewon1206@gmail.com>
2026-04-01 20:26:39 +0900

    feat: Main 클래스 식품 추가 및 식품 카테고리 생성

--- src/com/commerce/Main.java
+++ src/com/commerce/Main.java
@@ -9,6 +9,7 @@ public class Main {
         List<Category> categoryList = new ArrayList<>();
         List<Product> electronics = new ArrayList<>();
         List<Product> clothing = new ArrayList<>();
+        List<Product> foods = new ArrayList<>();
 
         Product product1 = new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 30);
         Product product2 = new Product("iPhone 16", 1350000, "Apple의 최신 스마트폰", 15);
@@ -19,6 +20,10 @@ public class Main {
         Product product6 = new Product("슬림컷 생지 데님 팬츠", 58000, "슬림한 라인이 돋보이는 데님", 7);
         Product product7 = new Product("오버핏 코튼 반팔 티셔츠", 29000, "탄탄한 20수 면 소재로 제작된 데일리 오버핏 티셔츠", 150);
 
+        Product product8 = new Product("치즈 듬뿍 페퍼로니 피자", 18900, "고소한 치즈와 짭짤한 페퍼로니의 환상 조합", 20);
+        Product product9 = new Product("프리미엄 한우 스테이크", 45000, "최상급 한우 채끝살로 만든 육즙 가득 스테이크", 10);
+        Product product10 = new Product("매콤 달콤 떡볶이 세트", 12000, "중독성 있는 소스와 쫄깃한 밀떡의 만남", 50);
+
         electronics.add(product1);
         electronics.add(product2);
         electronics.add(product3);
@@ -28,11 +33,17 @@ public class Main {
         clothing.add(product6);
         clothing.add(product7);
 
+        foods.add(product8);
+        foods.add(product9);
+        foods.add(product10);
+
         Category category1 = new Category(electronics, "전자제품");
         Category category2 = new Category(clothing, "의류");
+        Category category3 = new Category(foods, "식품");
 
         categoryList.add(category1);
         categoryList.add(category2);
+        categoryList.add(category3);
 
         CommerceSystem system = new CommerceSystem(categoryList);
         system.start();
